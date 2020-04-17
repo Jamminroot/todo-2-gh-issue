3e54f5ffaf3412486306b04f4aadacbc54d9318f
Dmitrii Chichuk <v-dmchic@microsoft.com>
2020-04-17 19:18:51 +0400

    Initial implementation

--- Program.cs
+++ Program.cs
@@ -1,79 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
+using RestSharp;
 
 namespace Todo2GhIssue
 {
+
+	[DataContract]
+	internal class Issue
+	{
+		[DataMember(Name = "title")]
+		public string Title;
+
+		[DataMember(Name = "number")]
+		public long Number;
+	}
+
 	internal class TodoItem
 	{
 		public int Line;
 		public string File;
 		public string Title;
-
-		public TodoItem(string title, int line, string file)
+		public TodoDiffType DiffType;
+		
+		public TodoItem(string title, int line, string file, TodoDiffType type)
 		{
 			Title = title;
 			Line = line;
 			File = file;
-		}
-
-		public override bool Equals(object comparand)
-		{
-			return comparand is TodoItem other && Line.Equals(other.Line) && File.Equals(other.File) && Title.Equals(other.Title);
+			DiffType = type;
 		}
 
 		public override string ToString()
 		{
 			return $"{Title} @ {File}:{Line}";
 		}
+
+		public object RequestBody(string ghIssueLabel=null)
+		{
+			return new {title = Title, body = Title + "\n" + File + ":" + Line, labels = $"[{ghIssueLabel}]"};
+		}
 	}
 
+	enum TodoDiffType
+	{
+		Addition,
+		Deletion
+	}
+	
 	internal class Program
 	{
-		private const string GhApiBase = @"https://api.github.com/repos/";
-		private const string CommentPatternToken = "{COMMENT_PATTERN}"; 
+		private const string ApiBase = @"https://api.github.com/repos/";
+		private const string CommentPatternToken = "{COMMENT_PATTERN}";
 		private const string TodoSignatureToken = "{TODO_SIGNATURE}";
 		private const string DiffHeaderPattern = @"(?<=diff\s--git\sa.*b.*).+";
 		private const string BlockStartPattern = @"((?<=^@@\s).+(?=\s@@))";
 		private const string LineNumPattern = @"(?<=\+).+";
 		private const string AdditionPattern = @"(?<=^\+).*";
 		private const string DeletionPattern = @"(?<=^-).*";
-		private const string TodoPatternStart = @"(?<="+CommentPatternToken+" ?"+TodoSignatureToken+"[ :]).+";
+		private const string TodoPatternStart = @"(?<=" + CommentPatternToken + " ?" + TodoSignatureToken + "[ :]).+";
 		
-		private static bool MatchTodo(string line, string commentPattern, string todoSignature, char[] trim, out string matchedValue)
+		private static IList<TodoItem> GetTodoItems(string[] diff, string commentPattern = @"\/\/", string todoSignature = "TODO",
+			char[] trimSeparators = null)
 		{
-			var match = Regex.Match(line, TodoPatternStart.Replace(CommentPatternToken, commentPattern).Replace(TodoSignatureToken, todoSignature));
-			matchedValue = match.Value.Trim(trim);
-			return match.Success;
-		}
-		private static Tuple<IList<TodoItem>, IList<TodoItem>> ListTodoItems(string[] diff, string[] lineCommentPatterns = null, string[] todoSignatures = null, char[] trimSeparators = null)
-		{
-			lineCommentPatterns ??= new[] {@"\/\/", @"\/\*"};
-			todoSignatures ??= new[] {"TODO"};
 			trimSeparators ??= new[] {' ', ':', ' ', '(', '"'};
-			var additions = new List<TodoItem>();
-			var deletions = new List<TodoItem>();
+			var todos = new List<TodoItem>();
 			var lineNumber = 0;
 			var currFile = "";
 			var rec = false;
 			foreach (var line in diff)
 			{
 				var headerMatch = Regex.Match(line, DiffHeaderPattern, RegexOptions.IgnoreCase);
-				if (headerMatch.Success)
-				{
-					currFile = headerMatch.Value;
-				}
+				if (headerMatch.Success) { currFile = headerMatch.Value; }
 				else
 				{
 					var blockStartMatch = Regex.Match(line, BlockStartPattern, RegexOptions.IgnoreCase);
 					if (blockStartMatch.Success)
 					{
 						var lineNumsMatch = Regex.Match(blockStartMatch.Value, LineNumPattern);
-						if (lineNumsMatch.Success)
-						{
-							lineNumber = int.Parse(lineNumsMatch.Groups[0].Value.Split(',')[0]);
-						}
+						if (lineNumsMatch.Success) { lineNumber = int.Parse(lineNumsMatch.Groups[0].Value.Split(',')[0]); }
 					}
 					else
 					{
@@ -81,84 +93,119 @@ namespace Todo2GhIssue
 						var additionMatch = Regex.Match(line, AdditionPattern, RegexOptions.IgnoreCase);
 						if (additionMatch.Success)
 						{
-							var addition = additionMatch.Value;
-							var found = false;
-							foreach (var commentPattern in lineCommentPatterns)
-							{
-								foreach (var todoSignature in todoSignatures)
-								{
-									if (!MatchTodo(addition, commentPattern, todoSignature, trimSeparators,out var todo)) continue;
-									found = true;
-									additions.Add(new TodoItem(todo, lineNumber, currFile));
-									break;
-								}
-								if (found) break;
-							}
+							var match = Regex.Match(line, TodoPatternStart.Replace(CommentPatternToken, commentPattern).Replace(TodoSignatureToken, todoSignature));
+							if (match.Success) { todos.Add(new TodoItem(match.Value.Trim(trimSeparators), lineNumber, currFile, TodoDiffType.Addition)); }
 						}
 						var deletionMatch = Regex.Match(line, DeletionPattern, RegexOptions.IgnoreCase);
 						if (deletionMatch.Success)
 						{
-							var deletion = deletionMatch.Value;
-							var found = false;
-							foreach (var commentPattern in lineCommentPatterns)
-							{
-								foreach (var todoSignature in todoSignatures)
-								{
-									if (!MatchTodo(deletion, commentPattern, todoSignature, trimSeparators,out var todo)) continue;
-									found = true;
-									deletions.Add(new TodoItem(todo, lineNumber, currFile));
-									break;
-								}
-								if (found) break;
-							}
+							var match = Regex.Match(line, TodoPatternStart.Replace(CommentPatternToken, commentPattern).Replace(TodoSignatureToken, todoSignature));
+							if (match.Success) { todos.Add(new TodoItem(match.Value.Trim(trimSeparators), lineNumber, currFile, TodoDiffType.Deletion)); }
 						}
 					}
 				}
 			}
-			return new Tuple<IList<TodoItem>, IList<TodoItem>>(additions, deletions);
+			return todos;
 		}
 
-		
+		private static string[] GetDiff(string repo, string token, string oldSha, string newSha)
+		{
+			var client = new RestClient($"{ApiBase}{repo}/compare/{oldSha}...{newSha}?access_token={token}") {Timeout = -1};
+			var request = new RestRequest(Method.GET);
+			request.AddHeader("Accept", "application/vnd.github.v3.diff");
+			var response = client.Execute(request);
+			if (response.StatusCode != HttpStatusCode.OK)
+			{
+				Console.WriteLine($"Failed to get diff.\n{response.StatusCode}\n{response.StatusDescription}\n{response.ResponseStatus}");
+				Environment.Exit(1);
+			}
+			return response.Content.Split('\n');
+		}
+
+		private static void HandleTodos(string repo, string token, string newSha, string ghIssueLabel, int timeout, IEnumerable<TodoItem> todos)
+		{
+			var activeIssues = GetActiveItems(repo, token);
+			var deletions = todos.Where(t => t.DiffType == TodoDiffType.Deletion).ToList();
+			var additions = todos.Where(t => t.DiffType == TodoDiffType.Addition).ToList();
+			foreach (var number in activeIssues.Where(i => deletions.Select(d => d.Title).Contains(i.Title)).Select(i=>i.Number))
+			{
+				var client = new RestClient($"{ApiBase}{repo}/issues/{number}?access_token={token}") {Timeout = -1};
+				var request = new RestRequest(Method.PATCH);
+				request.AddHeader("Accept", "application/json");
+				request.AddJsonBody(new {state="closed"});
+				var response = client.Execute(request);
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+					Console.WriteLine($"Failed to close GH issue.\n{response.StatusCode}\n{response.StatusDescription}\n{response.ResponseStatus}");
+					Environment.Exit(1);
+				}
+				request = new RestRequest(Method.POST);
+				request.AddHeader("Accept", "application/json");
+				request.AddJsonBody(new {state=$"Closed automatically with {newSha}"});
+				client = new RestClient($"{ApiBase}{repo}/issues/{number}/comments?access_token={token}") {Timeout = -1};
+				response = client.Execute(request);
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+					Console.WriteLine($"Failed to post GH comment.\n{response.StatusCode}\n{response.StatusDescription}\n{response.ResponseStatus}");
+					Environment.Exit(1);
+				}
+				Thread.Sleep(timeout);
+			}
+			foreach (var todoItem in additions)
+			{
+				var client = new RestClient($"{ApiBase}{repo}/issues?access_token={token}") {Timeout = -1};
+				var request = new RestRequest(Method.POST);
+				request.AddHeader("Accept", "application/json");
+				request.AddJsonBody(todoItem.RequestBody(ghIssueLabel));
+				var response = client.Execute(request);
+				if (response.StatusCode != HttpStatusCode.OK)
+				{
+					Console.WriteLine($"Failed to post GH comment.\n{response.StatusCode}\n{response.StatusDescription}\n{response.ResponseStatus}");
+					Environment.Exit(1);
+				}
+				Thread.Sleep(timeout);
+			}
+		}
+
+		private static IEnumerable<Issue> GetActiveItems(string repo, string token)
+		{
+			var client = new RestClient($"{ApiBase}{repo}/issues?access_token={token}") {Timeout = -1};
+			var request = new RestRequest(Method.GET);
+			request.AddHeader("Accept", "application/json");
+			var ser = new DataContractJsonSerializer(typeof(List<Issue>));
+			var response = client.Execute(request);
+			if (response.StatusCode != HttpStatusCode.OK)
+			{
+				Console.WriteLine($"Failed to get active items.\n{response.StatusCode}\n{response.StatusDescription}\n{response.ResponseStatus}");
+				Environment.Exit(1);
+			}
+			using var sr = new MemoryStream(Encoding.UTF8.GetBytes(response.Content));
+			var result = (List<Issue>)ser.ReadObject(sr);
+			return result;
+		}
 		
 		private static void Main(string[] args)
 		{
-
-			var repo = Environment.GetEnvironmentVariable("INPUT_REPO");
-			var before = Environment.GetEnvironmentVariable("INPUT_BEFORE");
-			var sha = Environment.GetEnvironmentVariable("INPUT_SHA");
-			var label = Environment.GetEnvironmentVariable("INPUT_LABEL");
-			var tokenParam = $"access_token={Environment.GetEnvironmentVariable("INPUT_TOKEN")}";
-			var todoLabel = Environment.GetEnvironmentVariable("TODO_LABEL");
-
-
-
-			var issuesApiUrl = $"{GhApiBase}{repo}/issues";
-			var issueHeaders = "{ 'Content-Type': 'application/json' }";
-			
-			var diff = File.ReadAllLines("c:\\temp\\sample_diff.txt");
-
-			var (additions, deletions) = ListTodoItems(diff);
-			Console.WriteLine("Additions:");
-			foreach (var todoItem in additions) { Console.WriteLine(todoItem); }
-			Console.WriteLine("Deletions:");
-			foreach (var todoItem in deletions) { Console.WriteLine(todoItem); }
-			/*title = issue['todo']
-        # Truncate the title if it's longer than 50 chars.
-        if len(title) > 50:
-            title = title[:50] + '...'
-        file = issue['file']
-        line = issue['line_num']
-        body = issue['body'] + '\n' + f'https://github.com/{repo}/blob/{sha}/{file}#L{line}'
-        new_issue_body = {'title': title, 'body': body, 'labels': ['todo']}
-        requests.post(url=issues_url, headers=issue_headers, params=params, data=json.dumps(new_issue_body))
-        # Don't add too many issues too quickly.
-        sleep(1)#1#
-		}
-		UploadWorkItems(todos);*/
-		}
-
-		private static void UploadWorkItems(IEnumerable<TodoItem> todos)
-		{
+			var repo = Environment.GetEnvironmentVariable("INPUT_REPOSITORY");
+			var oldSha = Environment.GetEnvironmentVariable("INPUT_OLD");
+			var newSha = Environment.GetEnvironmentVariable("INPUT_NEW");
+			var token = Environment.GetEnvironmentVariable("INPUT_TOKEN");
+			var todoLabel = Environment.GetEnvironmentVariable("INPUT_TODO");
+			var commentPattern = Environment.GetEnvironmentVariable("INPUT_COMMENT");
+			var ghIssueLabel = Environment.GetEnvironmentVariable("INPUT_LABEL");
+			var symbolsToTrim = Environment.GetEnvironmentVariable("INPUT_TRIM");
+			if (!int.TryParse(Environment.GetEnvironmentVariable("INPUT_TIMEOUT"), out var timeout))
+			{
+				timeout = 1000;
+			}
+			var diff = GetDiff(repo, token, oldSha, newSha);
+			var todos = GetTodoItems(diff, commentPattern, todoLabel, symbolsToTrim?.ToCharArray());
+			Console.WriteLine("Added TODOs:");
+			foreach (var todoItem in todos.Where(t=>t.DiffType==TodoDiffType.Addition)) { Console.WriteLine($"+\t{todoItem}"); }
+			Console.WriteLine("Removed TODOs:");
+			foreach (var todoItem in todos.Where(t=>t.DiffType==TodoDiffType.Deletion)) { Console.WriteLine($"-\t{todoItem}"); }
+			HandleTodos(repo, token, newSha, ghIssueLabel, timeout, todos);
+			Console.WriteLine("Finished updating issues.");
 		}
 	}
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This action run through your recent changes and close an issue if relevant `TODO
 ## Usage
 
 Create a workflow file in your .github/workflows directory as follows:
-Copy values for REPOSITORY, SHA_OLD, SHA_NEW, TOKEN from example, if your are going for default use case (running on the same repo the push even occured, and comparing with the most recent commit)
+Copy values for REPOSITORY, OLD, NEW, TOKEN from example, if your are going for default use case (running on the same repo the push even occured, and comparing with the most recent commit)
  
 ### todos.yaml
 
@@ -20,11 +20,14 @@ Copy values for REPOSITORY, SHA_OLD, SHA_NEW, TOKEN from example, if your are go
             uses: "jamminroot/todo-2-gh-issue@master"
             with:
               REPOSITORY: ${{ github.repository }}
-              SHA_OLD: ${{ github.event.before }}
-              SHA_NEW: ${{ github.sha }}
+              OLD: ${{ github.event.before }}
+              NEW: ${{ github.sha }}
               TOKEN: ${{ secrets.GITHUB_TOKEN }}
-              TODO_LABELS: ["TODO"]
-              COMMENT_PATTERNS: ["\/\/"]
+              TODO: "TODO"
+              COMMENT: "\/\/"
+              LABEL: "TODO"
+              TRIM: ",: ()\""
+              TIMEOUT: 1000
             id: "todo"
 
 ### Inputs
@@ -35,8 +38,11 @@ Copy values for REPOSITORY, SHA_OLD, SHA_NEW, TOKEN from example, if your are go
 | `SHA_OLD` | The SHA of the commit to compare with. |
 | `SHA_NEW` | The SHA of the commit comparand commit. |
 | `TOKEN` | The GitHub access token to allow us to retrieve, create and update issues. |
-| `TODO_LABELS` | The label that will be used to identify TODO comments.|
-| `COMMENT_PATTERNS` | Regex patterns used to identify start of comment. (`\/\/` C#'s `\\`) |
+| `TODO` | The label that will be used to identify TODO comments.|
+| `COMMENT` | Regex pattern used to identify start of comment. (`\/\/` for C#'s `\\`). |
+| `LABEL` | Label to add to github issue. |
+| `TRIM` | Set of characters (as a string) to be trimmed from resulting title. |
+| `TIMEOUT` | Delay between requests. |
 
 Note that todo labels will only be compared if they follow matching comment pattern. 
 Resulting regex with default C# values (e.g. `// TODO This is a comment`, where comment pattern is `\/\/` and TODO label is `TODO`) would be `(?<=\/\/?TODO[ :]).+`.
